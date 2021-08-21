1d7268a6fe7d2db8b3960ad75d53c31fc2ade227
bauti <jnemi@fie.undef.edu.ar>
2021-08-21 15:22:16 -0300

    REF - quickSort

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
 #include "EstDatos_Algoritmos.h"
 
 int main() {
 
-    counter = 1;
+    counter = 0;
 
     int LTest[8] = {2, 6, 5, 3, 8, 7, 1, 0};
+    printf("Original: ");
     printArray(LTest);
     //QuickArr(LTest, 0, 8);
     quickSort(LTest, 0, 8);
-    printArray(LTest);
-
-
 
     return 0;
 }
